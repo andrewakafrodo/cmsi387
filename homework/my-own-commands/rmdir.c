24d1093c6a6ccd2ee21d2e6aa8d34c83df9b230d
frodosamoa <akowalc1@lion.lmu.edu>
2013-02-19 23:16:55 -0800

    Update homework/my-own-commands/rmdir.c

--- homework/my-own-commands/rmdir.c
+++ homework/my-own-commands/rmdir.c
@@ -1,8 +1,4 @@
 /**
-<<<<<<< HEAD
-=======
- * 64BIT
->>>>>>> added changes
  * This program demonstrates invocation of the rmdir
  * system call (40) using the syscall function.
  */
@@ -15,15 +11,15 @@
 
 int main(int argc, char *argv[]) {
 
-<<<<<<< HEAD
   // I use command-line arguments for the command.
 
   int result = syscall(40, argv[1]);
-=======
-  int result = syscall(84, argv[1]);
->>>>>>> added changes
+  
+  // A result of -1 means that something did not go as planned.
+  // I included <errno.h> to give detailed errors for the syscall.
 
   if (result == -1) {
     printf("rmdir: failed to remove `%s`: %s\n", argv[1], strerror(errno));
   }
 }
+
