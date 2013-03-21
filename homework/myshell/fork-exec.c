9c13684efce37b4d8a8e1992999d38ca4af9b4b0
Andrew Kowalczyk <frodo@samtrefoil.(none)>
2013-03-21 07:34:23 -0700

    added secret-system-call, working on cd and the &

--- homework/myshell/fork-exec.c
+++ homework/myshell/fork-exec.c
@@ -7,11 +7,16 @@
  * This program demonstrates the use of the fork() and exec()
  * functions.
  */
-int main(int argc, char *argv[]) {
+int main () {
     /* String to hold the command to run. */
     char command[256];
+	char argument[256];
+	char and = '&';
     printf("Enter the command to run: ");
-    scanf("%s", command);
+    scanf("%s", command);//, argument);
+	printf("%s\n", command);
+	//printf("%s\n", argument);
+
 
     /* Variable that will store the fork result. */
     pid_t pid;
@@ -26,10 +31,12 @@ int main(int argc, char *argv[]) {
         /* Child process. */
         printf("Running...\n");
 	    if (strcmp("cd", command) == 0) {
-		    printf("We have cd\n");
-	    } else if ('&' == command[strlen(command)-1]) {
+		    //chdir(directory);
+	    } else if (strchr(command, and) != NULL) {
 		    printf("We have a &\n");
-	    } else {
+	    //} else if (strcmp("secret-system-call", command) == 0) {
+
+		} else {
         	execlp(command, command, NULL);
 	    }
     } else {
