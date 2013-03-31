66dfc2812a2923638b5e16d30693f759f9071223
frodosamoa <andrewrkowalczyk@gmail.com>
2013-03-30 21:25:02 -0700

    removed old comment with logic, and dondi's comment (sorry), added backgournd

--- homework/myshell/fork-exec.c
+++ homework/myshell/fork-exec.c
@@ -26,7 +26,11 @@ int main (void) {
     /* Perform the actual fork. */
     pid = fork();
 	if (pid > 0) {
-    
+        //if (backgound) {
+
+        //} else {   not background
+            //waitpid(pid, 0, 0);
+        //}
 	} else if (pid < 0) {
         /* Error condition. */
         fprintf(stderr, "Fork failed\n");
@@ -90,47 +94,3 @@ int analyze_commands (char *commands, char *pointer_array[MAX_CMDS]) {
 	pointer_array[index] = NULL;
     return 0;
 }
-
-// JD: This sketch is generally in the right direction, but I do have some
-//     points to raise at this stage (some of these issues are also already
-//     in your code above, so take note):
-//
-//     - Remember that fork *creates a new process*.  So you should put
-//       some thought into precisely when a new process is actually needed.
-//       Certainly you need a new process when executing a command, because
-//       otherwise that executable will wipe out the current code.  But do
-//       you need a process to cd?  To make your secret system call?
-//
-//     - Related is the issue of the &.  When should you catch it?  And if
-//       you do catch it, how should it affect command execution?  This
-//       relates to my third caution, which is...
-//
-//     - Make sure you fully understand what the exec function needs for
-//       its arguments, and process the user's command input accordingly.
-//       Remember, the way the & works, it affects solely whether the
-//       launched process runs concurrently.  It is not officially an
-//       "argument" that the new executable receives.
-//
-//     One last thing I guess---In the end, you are modeling the behavior
-//     of bash.  So, when in doubt, ask WWBD?  :)  Try something in bash
-//     to see what it does, and that will give you a sense for what your
-//     shell needs to do.
-/* 
-
-main(){
-infinite loop 
-    prompt for input
-    foo=fork()
-    if(foo>0) //parent process
-        if they used backgrounding &
-            continue. <-- this is the "keyword" continue, go to next loop iteration
-        else if they didn't "background," this is a normal command
-            waitpid(foo,0,0); //this is a system call in C.
-            //              try "man waitpid" in terminal to learn about waitpid
-    else if(foo==0) //child process
-        do the command (involves execvp, looking for pipes)
-    else //fork returned a negative value
-        printf("fork error\n");
-        return -1; //indicates error
-return 0 //indicates success
-} //end main() */
