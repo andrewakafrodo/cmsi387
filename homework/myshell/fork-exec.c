00d50be86143dc0928929fb5dcfa9a4c62ddf9d0
Andrew Kowalczyk <frodo@samtrefoil.(none)>
2013-04-01 19:38:07 -0700

    added a while loop to allow stdin

--- homework/myshell/fork-exec.c
+++ homework/myshell/fork-exec.c
@@ -4,61 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 
-#define MAX_CMDS 10
-
-/**
- * This program demonstrates the use of the fork() and exec()
- * functions.
- */
-
-int main (void) {
-    
-    /* Strings to hold the commands to run. */
-    char commands[100];
-    char *pointer_array[MAX_CMDS];
-    printf("Enter the commands to run: ");
-    fgets(commands, sizeof(commands), stdin);
-
-    /* Method to store commands and their options. */
-    analyze_commands(commands, pointer_array);
-
-    /* Variable that will store the fork result. */
-    pid_t pid;
-
-    /* Perform the actual fork. */
-    pid = fork();
-	if (pid > 0) {
-        //if (backgound) {
-
-        //} else {   not background
-            //waitpid(pid, 0, 0);
-        //}
-	} else if (pid < 0) {
-        /* Error condition. */
-        fprintf(stderr, "Fork failed\n");
-        return -1;
-    } else if (pid == 0) {
-        /* Child process. */
-		if (strcmp("cd", pointer_array[0]) == 0) {
-			int result = chdir(pointer_array[1]);
-		} else if (strcmp("&", pointer_array[0]) == 0) {
-		    //don't know if I need this or the (pid > 0) bit
-	    } else if (strcmp("secret-system-call", pointer_array[0]) == 0) {
-            int result = syscall(272);
-        } else if (strcmp("exit", pointer_array[0]) == 0) {
-            exit(0);
-		} else {
-        	execvp(pointer_array[0], pointer_array);
-	    }
-    } else {
-        /* Parent process. */ 
-        int result;
-        wait(&result);
-        printf("All done; result = %d\n", result);
-    }
-	
-    return 0;
-}
+#define MAX_CMDS 64
 
 int analyze_commands (char *commands, char *pointer_array[MAX_CMDS]) {
 	char *chars = commands, buffer[100] = {0};
@@ -74,9 +20,8 @@ int analyze_commands (char *commands, char *pointer_array[MAX_CMDS]) {
 		}
 		if (*chars == ' ') {
 			if (space == 0) {
-				pointer_array[index] = (char *)malloc(sizeof(char) * strlen(buffer) + 1);
+				pointer_array[index] = (char *)malloc(sizeof(char)*strlen(buffer)+1);
 				strncpy(pointer_array[index], buffer, strlen(buffer));
-				printf("%s\n", pointer_array[index]);
 				strncat(pointer_array[index], "\0", 1);
 				bzero(buffer, sizeof(buffer));
 				index++;
@@ -92,3 +37,73 @@ int analyze_commands (char *commands, char *pointer_array[MAX_CMDS]) {
 	pointer_array[index] = NULL;
     return 0;
 }
+
+void changeDirectory(char *directory) {
+
+}
+
+/**
+ * This program demonstrates the use of the fork() and exec()
+ * functions.
+ */
+
+int main (void) {
+    
+    /* Strings to hold the commands to run. */
+    char commands[100];
+    char *pointer_array[MAX_CMDS];
+	char *prompt = "=> ";
+	char *welcome = "Welcome to a basic shell, enter your commands.\n";
+	fputs(welcome, stdout);
+
+	while (!feof(stdin)) {
+
+	/* Put a the prompt to standard out */
+	fputs(prompt, stdout);
+    fgets(commands, sizeof(commands), stdin);
+
+    /* Method to store commands and their options. */
+    analyze_commands(commands, pointer_array);
+
+    /* Variable that will store the fork result. */
+    pid_t pid;
+
+    /* Perform the actual fork. */
+    pid = fork();
+	//if (pid > 0) {
+        //if (backgound) {
+
+        //} else {   not background
+            //waitpid(pid, 0, 0);
+        //}
+//	} else 
+	if (pid < 0) {
+        /* Error condition. */
+        fprintf(stderr, "Fork failed\n");
+        return -1;
+    } else if (pid == 0) {
+        /* Child process. */
+		if (strcmp("cd", pointer_array[0]) == 0) {
+			printf("%s\n", "we made it here");
+			changeDirectory(pointer_array[1]);
+		} else if (strcmp("&", pointer_array[0]) == 0) {
+		    //don't know if I need this or the (pid > 0) bit
+	    } else if (strcmp("secret-system-call", pointer_array[0]) == 0) {
+            int result = syscall(272);
+		} else if (strcmp("exit", pointer_array[1]) == 0) {
+			break;
+		} else {
+        	execvp(pointer_array[0], pointer_array);
+	    }
+    } else {
+        /* Parent process. */ 
+        int result;
+        wait(&result);
+        printf("All done; result = %d\n", result);
+    }
+	
+	}
+    return 0;
+}
+
+
