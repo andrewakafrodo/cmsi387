22a58573cf071611b6e7069315155a232ced9505
dondi <dondi@lmu.edu>
2013-04-30 00:21:41 -0700

    Provided feedback for HW 0423 with corresponding update to standards development report.

--- homework/paged-memory-address-translation/addressTranslation.c
+++ homework/paged-memory-address-translation/addressTranslation.c
@@ -13,7 +13,8 @@ void setPageTable(pagetable *pt) {
 }
 
 int getPhysical(int logical) {
-	/** 
+    // JD: ACK!!!! TABS!!!  Run, run for the hills!!!  >:-)
+	/**
 	 * These are the left most 4 bits and the right most four bits.
 	 */
 	int leftBits = ((logical & PAGEMASK) >> PAGEBITS);
@@ -23,7 +24,11 @@ int getPhysical(int logical) {
 	 * These test if logical is out of range and if the page table
 	 * valid integer is 0.
 	 */
-	if (logical < 0 || logical >= 256) return ERR_OUT_OF_RANGE;
+    // JD: Use curly braces even for a single-statement block:
+	if (logical < 0 || logical >= 256) {
+        return ERR_OUT_OF_RANGE;
+    }
+    // JD: Same here about curly braces.
 	if (ptr[leftBits].valid == 0) return ERR_INVALID;
 
     return (((ptr[leftBits].frame) << PAGEBITS) + rightBits);
