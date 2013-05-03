5eebb98afa6035e250d6878af5e653aeba819aad
frodosamoa <andrewrkowalczyk@gmail.com>
2013-05-02 20:38:48 -0700

    updated with a pseudocode understanding of what is to happen in the code

--- homework/dining-philosophers/philosophers.c
+++ homework/dining-philosophers/philosophers.c
@@ -1,6 +1,75 @@
-#define N 5
-#define THINKING 0
-#define HUNGRY 1
-#define EATING 2
+#include <pthread.h>
+#include <semaphore.h>
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
 
+void eat () {
+
+	nom nom nom
+
+}
+
+void think () {
+
+	what is life?
+
+}
+
+void getLeftChopstick () {
+
+	if (left chops stick available) {
+		get it!
+	} if not {
+		try again in a bit;
+	}
+
+}
+
+void getRightChopstick () {
+
+	if (right chop stick available) {
+		get it!
+	} if not {
+		try again in a bit;
+	}
+
+}
+
+void putLeftChopstick () {
+
+	releaseLeftChopstick();
+
+}
+
+void putRightChopstick () {
+
+	releaseRightChopstick();
+
+}
+
+void philosophize () {
+
+	while (true) {
+		think ();
+		getLeftChopstick();
+		getRightChopstick();
+		eat();
+		putLeftChopstick();
+		putRightChopstick();
+	}
+
+}
+
+int main () {
+
+	createPhilosophers();
+	creatChopsticks();
+
+	while (true) {
+		philosophize;
+	}
+
+}
