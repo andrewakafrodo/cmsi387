17895778db61de83d22b88fed84bb14084e4a142
frodosamoa <andrewrkowalczyk@gmail.com>
2013-05-06 00:30:48 -0700

    defined constants for easier checking, delcared all of the arrays for the information to be held in, consolidated get and releasing chopstick functions, got the main method much more clearer

--- homework/dining-philosophers/philosophers.c
+++ homework/dining-philosophers/philosophers.c
@@ -7,82 +7,98 @@
 #include <stdio.h>
 
 #define NUM 5
+#define TRUE 1
+#define FALSE !TRUE
+#define HUNGRY 0
+#define EATING 1
+#define THINKING 2 
 
+void getChopsticks (int philosopher);
+void releaseChopsticks (int philosopher);
+int philosopher_number[NUM];
+int philosopher_state[NUM];
+int chopstick_state[NUM];
 pthread_mutex_t chopsticks[NUM];
 
-void eat (int id) {
+/**
+ *  This lets our philosphers eat. They get the chopsticks and then commence eating.
+ */ 
 
-	getLeftChopstick();
-	getRightChopstick();
-	nom nom nom
-	releaseLeftChopstick();
-	releaseRightChopstick();
+void eat (int philosopher) {
 
-}
+	getChopsticks(philosopher);
 
-void think (int id) {
 
-	what is life?
+	releaseChopsticks(philosopher);
 
 }
 
-void getLeftChopstick (int id) {
+/**
+ *  This lets our philosphers "think".
+ */ 
 
-	if (left chops stick available) {
-		get it!
-	} if not {
-		try again in a bit;
+void think (int philosopher) {
+
+	int i;
+
+	usleep(40000);
+
+}
+
+/**
+ *  This lets our philosphers get the chopsticks.
+ */ 
+
+void getChopsticks (int philosopher) {
+
+
+}
+
+/**
+ *  This lets our philosphers release the chopsticks.
+ */ 
+
+void releaseChopsticks (int philosopher) {
+
+
+}
+
+/**
+ *  This function will let our philosphers philosophize (eating and thinking).
+ */ 
+
+void philosophize (int philosopher) {
+
+	while (TRUE) {
+		think(philosopher);
+		eat(philosopher);
 	}
 
 }
 
-void getRightChopstick (int id) {
-
-	if (right chop stick available) {
-		get it!
-	} if not {
-		try again in a bit;
-	}
-
-}
-
-void releaseLeftChopstick (int id) {
-
-	release the left chopstick
-
-}
-
-void releaseRightChopstick (int id) {
-
-	release the right chopstick
-
-}
-
-void philosophize (int id) {
-
-	while (true) {
-		think ();
-		eat();
-	}
-
-}
+/**
+ *	Main method to create all of the threads and run them.
+ */
 
 int main () {
 
 	/* Reusable i, integer array for id, and our threads. */
 	int i, id[NUM];
-	pthread_t tid[NUM];
+	pthread_t philosphers[NUM];
 
 	/* Let us initialize the mutexes. */
 	for (i = 0; i < NUM; i++) {
-		pthread_mutex_init(chopsticks[i], 0);
+		philosopher_state[i] = THINKING;
+		philosopher_number[i] = i;
+		chopstick_state[i] = 0;
+		pthread_mutex_init(&chopsticks[i], NULL);
+		pthread_create(&philosphers[i], NULL, philosophize, philosopher_number[i]);
 	}
 
 	/* Let us start the threads. */
 	for (i = 0; i < NUM; i++) {
-		pthread_create(tid, NULL, philosophize, id)
+		pthread_join(&philosphers[i], NULL);
 	}
-
 
 	/* We shouldn't ever get here, but let us make the compiler happy. */
 	return 0;
